e52bde14d1dbbfa8450184092ff4e37029e4086e
tjang <tjang@fusiongenomics.com>
2017-03-14 17:26:22 -0700

    first commit

---
+++ Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AQLiteWeb.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string School { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
